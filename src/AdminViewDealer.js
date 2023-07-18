a304902214ca938ee2088344fd504c6926085448
GTM4Health <129034982+GTM4Health@users.noreply.github.com>
2023-07-18 20:09:24 +0530

    Cascade Menubar & Add Products v1.2.14

--- src/AdminViewDealer.js
+++ src/AdminViewDealer.js
@@ -138,7 +138,7 @@ const DealerPortal = () => {
         <div className="dashboard">
           <AdminMenuBar />
           <div className="page-title">
-            <h1 className="page-title-child">Dealers-Distributors</h1>
+            <h1 className="page-title-child">MedTech-Companies</h1>
           </div>
           <div className="filter-container">
             <label htmlFor="state-select">State:</label>
@@ -161,7 +161,7 @@ const DealerPortal = () => {
             </select>
           </div>
           <div className="page-display">
-            <h4 className="total-rows">Total Dealers-Distributors = {totalRows}</h4>
+            <h4 className="total-rows">Total MedTech-Companies = {totalRows}</h4>
             <h4 className="right">
               <i>
                 Displaying Page {currentPage} of {totalPages}
@@ -183,6 +183,7 @@ const DealerPortal = () => {
                   <th>Role</th>
                   <th>Contact Email</th>
                   <th>Contact Number</th>
+                  <th>GST No.</th>
                   <th>Actions</th>
                 </tr>
               </thead>
@@ -200,6 +201,7 @@ const DealerPortal = () => {
                     <td>{dealer.role}</td>
                     <td>{dealer.mail}</td>
                     <td>{dealer.phone}</td>
+                    <td>{dealer.GST}</td>
                     <td>
                       <button className="edit-button" onClick={() => handleEditDealer(dealer)}>
                         <i className="fas fa-pencil-alt"></i>
