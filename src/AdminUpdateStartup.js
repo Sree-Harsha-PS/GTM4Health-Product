0b0a0d42083525c56fd14b5adbee63279104c2ab
GTM4Health <129034982+GTM4Health@users.noreply.github.com>
2023-07-20 19:08:14 +0530

    Install Startup Portal v1.2.16

---
+++ src/AdminUpdateStartup.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const EditStartupForm = ({ startup, onUpdate, onCancel }) => {
+  const [startupName, setStartupName] = useState(startup.startupName);
+  const [website, setWebsite] = useState(startup.website);
+  const [productStage, setProductStage] = useState(startup.productStage);
+  const [domain, setDomain] = useState(startup.domain);
+
+  useEffect(() => {
+    setStartupName(startup.startupName);
+    setWebsite(startup.website);
+    setProductStage(startup.productStage);
+    setDomain(startup.domain);
+  }, [startup]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const updatedData = {
+      startupName,
+      website,
+      productStage,
+      domain,
+    };
+    onUpdate(startup._id, updatedData);
+  };
+
+  return (
+    <div className="edit-form">
+      <h2>Edit Startup</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="startupName">Startup Name</label>
+          <input
+            type="text"
+            id="startupName"
+            value={startupName}
+            onChange={(e) => setStartupName(e.target.value)}
+            placeholder="Startup Name"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="website">Website</label>
+          <input
+            type="text"
+            id="website"
+            value={website}
+            onChange={(e) => setWebsite(e.target.value)}
+            placeholder="Website"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="productStage">Product Stage</label>
+          <select
+            id="productStage"
+            value={productStage}
+            onChange={(e) => setProductStage(e.target.value)}
+          >
+            <option value="MVP">MVP</option>
+            <option value="Early Adopters">Early Adopters</option>
+            <option value="Commercialised">Commercialised</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="domain">Domain</label>
+          <select
+            id="domain"
+            value={domain}
+            onChange={(e) => setDomain(e.target.value)}
+          >
+            <option value="Maternal Child Healthcare (MCH)">Maternal Child Healthcare (MCH)</option>
+            <option value="Non Communicable Diseases (NCD)">Non Communicable Diseases (NCD)</option>
+            <option value="Assistive Healthcare Technologies">Assistive Healthcare Technologies</option>
+            <option value="Chronic Care">Chronic Care</option>
+            <option value="Hearing Diagnostics">Hearing Diagnostics</option>
+            <option value="IVF">IVF</option>
+            <option value="Colostomy">Colostomy</option>
+            <option value="Rehabilitation Solutions">Rehabilitation Solutions</option>
+            <option value="Cancer Care">Cancer Care</option>
+            <option value="Kidney Care">Kidney Care</option>
+          </select>
+        </div>
+        <div className="button-group">
+          <button type="submit" className="btn-primary">
+            Update
+          </button>
+          <button type="button" className="btn-secondary" onClick={onCancel}>
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default EditStartupForm;
